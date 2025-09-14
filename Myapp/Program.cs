f65af172b57189d1f94ee7618381d765aa421476
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2025-09-14 11:10:25 +0530

    Initial Commit

---
+++ Myapp/Program.cs
@@ -0,0 +1,39 @@
+using MyApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MyAppContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnectionString"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnectionString"))
+    )
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
