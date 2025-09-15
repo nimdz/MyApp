22f3bd470b74febe839b13b0ab151094912d63d3
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2025-09-15 15:01:26 +0530

    add DbContext

---
+++ Myapp/Data/MyAppContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Myapp.Models;
+
+namespace MyApp.Data
+{
+    public class MyAppContext : DbContext
+    {
+
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
+
+
+        public DbSet<Item> Items { get; set; }
+
+    }
+
+}
